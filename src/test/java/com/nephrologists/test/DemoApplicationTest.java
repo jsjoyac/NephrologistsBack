589d896c45a75704e9866efa0f41cbcbd3ceaa54
jsjoyac <juansjoyac@gmail.com>
2020-03-30 02:13:58 -0500

    ajustes test

--- src/test/java/com/nephrologists/test/DemoApplicationTest.java
+++ src/test/java/com/nephrologists/test/DemoApplicationTest.java
@@ -1,10 +1,10 @@
-package com.tarjetascredito.demo;
+package com.nephrologists.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TarjetasCreditoDemoApplicationTests {
+class DemoApplicationTest {
 
 	@Test
 	void contextLoads() {
